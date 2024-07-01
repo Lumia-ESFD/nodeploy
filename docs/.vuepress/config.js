2e7f016f79360e0a18da1216a7e215c2824d2f58
Lumia-ESFD <git@esfd.eu.org>
2024-07-01 12:25:43 +0800

    1msg

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  title: '瓜农的文档',
+  description: '曾经是码农，现在是瓜农',
+  base: '/lumia-esfd.github.io/',
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
+  themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: '过程文档', 
+                items: [
+                    { text: '如何种瓜', link: 'https://github.com/' },
+                    { text: '如何码砖', link: 'https://juejin.cn/' }
+                ]
+            }
+        ],
+		sidebar: [
+            {
+                title: '欢迎学习',
+                path: '/',
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "学前必读", path: "/" }
+                ]
+            },
+            {
+              title: "基础学习",
+              path: '/handbook/ConditionalTypes',
+              collapsable: false, // 不折叠
+              children: [
+                { title: "准备种瓜", path: "/handbook/ConditionalTypes" },
+                { title: "翻地技巧", path: "/handbook/Generics" }
+              ],
+            }
+          ]
+    }
+}
